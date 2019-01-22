46d6b0afdbba3686c608ee89805fb665351637dd
ofchen <39107565+ofchen@users.noreply.github.com>
2019-01-22 23:27:32 +0800

    Add files via upload

---
+++ basic/ListNode.java
@@ -0,0 +1,10 @@
+package LeetCode.basic;
+//����Ļ������
+public class ListNode {
+	private int val;
+	public ListNode next;
+	public ListNode(int x) { this.val = x; }
+	public int getVal(){
+		return this.val;
+	}
+}
---
+++ basic/TreeNode.java
@@ -0,0 +1,11 @@
+package LeetCode.basic;
+//���Ļ������
+public class TreeNode {
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
+	public TreeNode(int x) { this.val = x; }
+	public int getVal(){
+		return this.val;
+	}
+}
